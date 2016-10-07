ede78c294fb93bdab445f0b86d7c96367ba54a29
Sunny Shah <Sunny.Shah@goibibo.com>
2016-10-07 19:20:24 +0530

    Minor modifications in the merge contract

--- src/main/scala/com/goibibo/mysqlRedshiftLoader/package.scala
+++ src/main/scala/com/goibibo/mysqlRedshiftLoader/package.scala
@@ -52,23 +52,23 @@ package object mysqlRedshiftLoader {
     //mergeKey: If mergeKey is not provided by default code uses primaryKey of the table as the mergeKey
     case class IncrementalSettings(whereCondition:String, 
                                     shallMerge:Boolean = false, 
-                                    mergeKey:Option[String] = None
+                                    mergeKey:Option[String] = None,
                                     shallVaccumAfterLoad:Boolean = false )
     
     //Defaults, 
     //If shallSplit = None then shallSplit = true
     
-    //If shallCreateTable = None && incrementalSettings = None
-    //    then shallCreateTable is true
-    //If shallCreateTable = None && incrementalSettings != None
-    //    then shallCreateTable is false
-    //If shallCreateTable != None
-    //    shallCreateTable = shallCreateTable.get
+    //If shallOverwrite = None && incrementalSettings = None
+    //    then shallOverwrite is true
+    //If shallOverwrite = None && incrementalSettings != None
+    //    then shallOverwrite is false
+    //If shallOverwrite != None
+    //    shallOverwrite = shallOverwrite.get
 
     //mapPartitions => set this with caution, If set to very high number, This can crash the database replica
     //reducePartitions => Parallism is good for Redshift, Set this to >12, If this is same as the mapPartitions then 
     //                      a reduce phase will be saved
-    case class InternalConfig( shallSplit:Option[Boolean] = None, shallCreateTable:Option[Boolean] = None, 
+    case class InternalConfig( shallSplit:Option[Boolean] = None, shallOverwrite:Option[Boolean] = None, 
         incrementalSettings:Option[IncrementalSettings] = None,  mapPartitions:Int = 12, reducePartitions:Int = 12)
 
     case class AppParams(mysqlConfPath: String, s3ConfPath: String, 
