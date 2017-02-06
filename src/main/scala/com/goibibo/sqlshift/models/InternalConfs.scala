ded4fbeb145b36d1679212eb3d06477eb76ba0e9
Shivam Sharma <shivam.sharma@goibibo.com>
2017-02-06 18:39:01 +0530

    Added automatic generation of failed configuration

--- src/main/scala/com/goibibo/sqlshift/models/InternalConfs.scala
+++ src/main/scala/com/goibibo/sqlshift/models/InternalConfs.scala
@@ -69,11 +69,11 @@ private[sqlshift] object InternalConfs {
                               mapPartitions: Option[Int] = None,
                               reducePartitions: Option[Int] = None) extends InternalConf
 
-    case class MigrationTime(loadTime: Long,
-                             storeTime: Long) {
+    case class MigrationTime(loadTime: Double,
+                             storeTime: Double) {
 
         override def toString: String = {
-            s"""{ LoadTime: $loadTime ms, StoreTime: $storeTime ms}"""
+            s"""{ LoadTime: $loadTime sec, StoreTime: $storeTime sec}"""
         }
     }
 
