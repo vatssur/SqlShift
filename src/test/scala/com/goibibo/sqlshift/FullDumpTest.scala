4d1c2f67d9160f7e4fd7909c8ae18edb666679b5
Shivam Sharma <28shivamsharma@gmail.com>
2018-05-15 02:51:28 +0530

    Added redshift table cleaning

---
+++ src/test/scala/com/goibibo/sqlshift/FullDumpTest.scala
@@ -0,0 +1,142 @@
+package com.goibibo.sqlshift
+
+import java.net.URL
+
+import com.goibibo.sqlshift.commons.Util
+import com.goibibo.sqlshift.models.Configurations
+import com.goibibo.sqlshift.services.{DockerMySQLService, DockerZookeeperService}
+import com.typesafe.config.{Config, ConfigFactory}
+import com.whisk.docker.impl.spotify.DockerKitSpotify
+import com.whisk.docker.scalatest.DockerTestKit
+import org.apache.spark.sql.DataFrame
+import org.scalatest.time.{Second, Seconds, Span}
+import org.scalatest.{FlatSpec, GivenWhenThen, Matchers}
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.util.{Failure, Success}
+
+/**
+  * Project: sqlshift
+  * Author: shivamsharma
+  * Date: 5/8/18.
+  */
+class FullDumpTest extends FlatSpec
+        with Matchers
+        with GivenWhenThen
+        with DockerTestKit
+        with DockerKitSpotify
+        with DockerMySQLService
+        with DockerZookeeperService
+        with SparkNRedshiftUtil {
+
+    private val logger: Logger = LoggerFactory.getLogger(this.getClass)
+    implicit val pc: PatienceConfig = PatienceConfig(Span(20, Seconds), Span(1, Second))
+
+    private val fixtures = new {
+        val config: Config = ConfigFactory.load("testsuite")
+        val redshift: Config = config.getConfig("redshift")
+        val recordsFileName: String = config.getString("table.recordsFileName")
+        val recordsFile: URL = this.getClass.getClassLoader.getResource(recordsFileName)
+
+        private val fullDumpTables: Config = config.getConfig("tableNames.fullDump")
+        val nonSplitTable: String = fullDumpTables.getString("nonSplitTable")
+        val splitTableWithoutDistKey: String = fullDumpTables.getString("splitTableWithoutDistKey")
+        val splitTableWithDistKey: String = fullDumpTables.getString("splitTableWithDistKey")
+        val listOfTables = List(nonSplitTable, splitTableWithoutDistKey, splitTableWithDistKey)
+        var pAppConfiguration: Configurations.PAppConfiguration = _
+    }
+
+    private trait PSVData {
+        val psvFullDumpRdd: DataFrame = sqlContext
+                .read
+                .format("com.databricks.spark.csv")
+                .option("header", "true")
+                .option("inferSchema", "true")
+                .option("delimiter", "|")
+                .load(fixtures.recordsFile.getFile)
+    }
+
+    def setUpMySQL(): Unit = {
+        import fixtures._
+        logger.info(s"Inserting records in MySQL from file: $recordsFileName")
+        isContainerReady(mySQLContainer) onComplete {
+            case Success(posts) =>
+                listOfTables.foreach { tableName =>
+                    MySQLUtil.createTableAndInsertRecords(config, tableName, recordsFile)
+                }
+            case Failure(t) => logger.error("Error occurred making container ready", t)
+        }
+        logger.info("Insertion Done!!!")
+    }
+
+    def startSqlShift(): Configurations.PAppConfiguration = {
+
+        val url = this.getClass.getClassLoader.getResource("full_dump.conf")
+        fixtures.pAppConfiguration = Util.getAppConfigurations(url.getFile)
+        SQLShift.start(sqlContext, fixtures.pAppConfiguration, 0)
+    }
+
+    override def beforeAll(): Unit = {
+        super.beforeAll()
+        setUpMySQL()
+        val pAppConfiguration = startSqlShift()
+        logger.info("\n{}", Util.formattedInfoSection(pAppConfiguration))
+    }
+
+    "Full Dump(Non Splittable) to redshift" should "have equal count" in new PSVData {
+        import fixtures._
+
+        val tableName: String = config.getString("redshift.schema") + "." + nonSplitTable
+        logger.info(s"Fetching table $tableName from redshift.")
+        val redshiftFullDumpDataFrame: DataFrame = readTableFromRedshift(config, tableName)
+
+        redshiftFullDumpDataFrame.count should equal(psvFullDumpRdd.count)
+    }
+
+    "Full Dump(Non Splittable) to redshift" should "have same records" in new PSVData {
+        import fixtures._
+
+        val tableName: String = config.getString("redshift.schema") + "." + nonSplitTable
+        logger.info(s"Fetching table $tableName from redshift.")
+        val redshiftFullDumpDataFrame: DataFrame = readTableFromRedshift(config, tableName)
+
+        redshiftFullDumpDataFrame.except(psvFullDumpRdd).count() should equal(0)
+    }
+
+    "Full Dump(Splittable With DistKey) to redshift" should "have equal count" in new PSVData {
+        import fixtures._
+
+        val tableName: String = config.getString("redshift.schema") + "." + splitTableWithDistKey
+        logger.info(s"Fetching table $tableName from redshift.")
+        val redshiftFullDumpDataFrame: DataFrame = readTableFromRedshift(config, tableName)
+
+        redshiftFullDumpDataFrame.count should equal(psvFullDumpRdd.count)
+    }
+
+    "Full Dump(Splittable With DistKey) to redshift" should "have same records" in new PSVData {
+        import fixtures._
+
+        val tableName: String = config.getString("redshift.schema") + "." + splitTableWithDistKey
+        logger.info(s"Fetching table $tableName from redshift.")
+        val redshiftFullDumpDataFrame: DataFrame = readTableFromRedshift(config, tableName)
+
+        redshiftFullDumpDataFrame.except(psvFullDumpRdd).count() should equal(0)
+    }
+
+    "Full Dump(Splittable Without DistKey) to redshift" should "fail if table not have primary key" in new PSVData {
+        import fixtures._
+
+        val tableName: String = config.getString("redshift.schema") + "." + splitTableWithoutDistKey
+        logger.info(s"Fetching table $tableName from redshift.")
+        val redshiftFullDumpDataFrame: DataFrame = readTableFromRedshift(config, tableName)
+
+        redshiftFullDumpDataFrame.count should equal(psvFullDumpRdd.count)
+    }
+
+    override def afterAll(): Unit = {
+        import fixtures._
+        super.afterAll()
+        val redshiftTablesToClean = listOfTables.map(table => config.getString("redshift.schema") + "." + table)
+        dropTableRedshift(config, redshiftTablesToClean: _*)
+    }
+}
--- src/test/scala/com/goibibo/sqlshift/SparkNRedshiftUtil.scala
+++ src/test/scala/com/goibibo/sqlshift/SparkNRedshiftUtil.scala
@@ -1,16 +1,35 @@
 package com.goibibo.sqlshift
 
+import java.sql.{Connection, DriverManager}
+import java.util.Properties
+
 import com.databricks.spark.redshift.RedshiftReaderM
+import com.goibibo.sqlshift.MySQLUtil.{getMySQLConnection, logger}
 import com.typesafe.config.Config
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.io.Source
 
 /**
   * Project: sqlshift
   * Author: shivamsharma
   * Date: 5/10/18.
   */
-trait SparkUtil {
+trait SparkNRedshiftUtil {
+    private val logger: Logger = LoggerFactory.getLogger(this.getClass)
+
+    private def getRedshiftConnection(config: Config): Connection = {
+        val mysql = config.getConfig("redshift")
+        val connectionProps = new Properties()
+        connectionProps.put("user", mysql.getString("username"))
+        connectionProps.put("password", mysql.getString("password"))
+        val jdbcUrl = s"jdbc:redshift://${mysql.getString("hostname")}:${mysql.getInt("portno")}/${mysql.getString("database")}?useSSL=false"
+        Class.forName("com.amazon.redshift.jdbc4.Driver")
+        DriverManager.getConnection(jdbcUrl, connectionProps)
+    }
+
     def getSparkContext: (SparkContext, SQLContext) = {
         val sparkConf: SparkConf = new SparkConf().setAppName("Full Dump Testing").setMaster("local")
         val sc: SparkContext = new SparkContext(sparkConf)
@@ -34,5 +53,19 @@ trait SparkUtil {
             "aws_iam_role" -> config.getString("redshift.iamRole")
         )
         RedshiftReaderM.getDataFrameForConfig(options, sc, sqlContext)
+    }
+
+    def dropTableRedshift(config: Config, tables: String*): Unit ={
+        logger.info("Droping table: {}", tables)
+        val conn = getRedshiftConnection(config)
+        val statement = conn.createStatement()
+        try {
+            val dropTableQuery = s"""DROP TABLE ${tables.mkString(",")}"""
+            logger.info("Running query: {}", dropTableQuery)
+            statement.executeUpdate(dropTableQuery)
+        } finally {
+            statement.close()
+            conn.close()
+        }
     }
 }
